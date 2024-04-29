2afbff91c852bd5e13432a0a72a3676de877f5ca
vplotnikov16 <vplotnikov16@yandex.ru>
2024-04-29 10:19:31 +0500

    Заданы бизнес-правила, реализована обработка событий, вызываемых действиями пользователя

--- model.py
+++ model.py
@@ -1,17 +1,57 @@
+from typing import Literal
 from PyQt5 import QtCore
+from PyQt5.QtGui import QCloseEvent
 from PyQt5.QtWidgets import QWidget
+from os.path import isfile
 
 
 class Model(QWidget):
     observers = QtCore.pyqtSignal()
+    log = QtCore.pyqtSignal(str)
+    value_A = 0
+    value_B = 0
+    value_C = 0
 
-    def __init__(self, value=0):
+    def __init__(self):
         super().__init__()
-        self.value = 0
 
-    def set_value(self, value):
-        self.value = value
+    def init_values(self):
+        self.load_values()
+
+    def load_values(self):
+        if isfile("values.txt"):
+            with open("values.txt", "r") as file:
+                value_A, value_B, value_C = map(int, file.readlines())
+                self.set_value(value_A, value_B, value_C)
+
+    def save_values(self):
+        with open("values.txt", "w") as file:
+            file.write(f"{self.value_A}\n{self.value_B}\n{self.value_C}")
+
+    def set_value(self, value_A: int | None = None, value_B: int | None = None, value_C: int | None = None):
+        if value_A:
+            self.value_A = value_A
+            if self.value_A > self.value_B:
+                self.value_B = self.value_A
+            if self.value_C < self.value_B:
+                self.value_C = self.value_B
+        if value_C:
+            self.value_C = value_C
+            if self.value_C < self.value_B:
+                self.value_B = self.value_C
+            if self.value_A > self.value_B:
+                self.value_A = self.value_B
+        if value_B:
+            if self.value_A <= value_B <= self.value_C:
+                self.value_B = value_B
+            else:
+                self.log.emit(
+                    f"Недопустимое значение B = {value_B}: число B должно быть от {self.value_A} до {self.value_C}")
         self.observers.emit()
 
-    def get_value(self):
-        return self.value
+    def get_value(self, value_type: Literal["A", "B", "C"]):
+        return self.value_A if value_type == "A" else self.value_B if value_type == "B" else self.value_C
+
+    def closeEvent(self, event: QCloseEvent):
+        super().closeEvent(event)
+        self.save_values()
--- windows/scripts/mainwindow.py
+++ windows/scripts/mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtGui import QKeyEvent, QCloseEvent
 from PyQt5.QtWidgets import QMainWindow
 
 from model import Model
@@ -12,7 +12,72 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.model = Model()
-        self.model.observers.connect(self.value_changed)
+        self.model.observers.connect(self.update_from_model)
+        self.model.log.connect(self.show_message)
+        self.model.init_values()
 
-    def value_changed(self):
-        pass
+        self.lineEdit_A.textEdited.connect(self.lineEdit_A_textEdited)
+        self.lineEdit_B.textEdited.connect(self.lineEdit_B_textEdited)
+        self.lineEdit_C.textEdited.connect(self.lineEdit_C_textEdited)
+
+        self.spinBox_A.valueChanged.connect(self.spinBox_A_valueChanged)
+        self.spinBox_B.valueChanged.connect(self.spinBox_B_valueChanged)
+        self.spinBox_C.valueChanged.connect(self.spinBox_C_valueChanged)
+
+        self.horizontalSlider_A.valueChanged.connect(self.horizontalSlider_A_valueChanged)
+        self.horizontalSlider_B.valueChanged.connect(self.horizontalSlider_B_valueChanged)
+        self.horizontalSlider_C.valueChanged.connect(self.horizontalSlider_C_valueChanged)
+
+    def update_from_model(self):
+        value_A = self.model.get_value("A")
+        value_B = self.model.get_value("B")
+        value_C = self.model.get_value("C")
+
+        self.lineEdit_A.setText(str(value_A))
+        self.lineEdit_B.setText(str(value_B))
+        self.lineEdit_C.setText(str(value_C))
+
+        self.spinBox_A.setValue(value_A)
+        self.spinBox_B.setValue(value_B)
+        self.spinBox_C.setValue(value_C)
+
+        self.horizontalSlider_A.setValue(value_A)
+        self.horizontalSlider_B.setValue(value_B)
+        self.horizontalSlider_C.setValue(value_C)
+
+    def show_message(self, text):
+        self.statusbar.showMessage(text, 3000)
+
+    def lineEdit_A_textEdited(self, text):
+        if text.isnumeric():
+            self.model.set_value(value_A=int(text))
+
+    def lineEdit_B_textEdited(self, text):
+        if text.isnumeric():
+            self.model.set_value(value_B=int(text))
+
+    def lineEdit_C_textEdited(self, text):
+        if text.isnumeric():
+            self.model.set_value(value_C=int(text))
+
+    def spinBox_A_valueChanged(self, value):
+        self.model.set_value(value_A=value)
+
+    def spinBox_B_valueChanged(self, value):
+        self.model.set_value(value_B=value)
+
+    def spinBox_C_valueChanged(self, value):
+        self.model.set_value(value_C=value)
+
+    def horizontalSlider_A_valueChanged(self, value):
+        self.model.set_value(value_A=value)
+
+    def horizontalSlider_B_valueChanged(self, value):
+        self.model.set_value(value_B=value)
+
+    def horizontalSlider_C_valueChanged(self, value):
+        self.model.set_value(value_C=value)
+
+    def closeEvent(self, event: QCloseEvent):
+        super().closeEvent(event)
+        self.model.close()
